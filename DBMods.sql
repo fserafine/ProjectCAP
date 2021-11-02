667ea441b7224ed6ed670802c8c37ab435215e15
Frank Serafine <theserafine@gmail.com>
2021-11-02 00:28:07 -0400

    Establishing daily commit/push cycle for continuous updates. Modified DBMods and README, as well as Jupyter notebook and Tableau files.

--- DBMods.sql
+++ DBMods.sql
@@ -1,12 +1,28 @@
 -- Add race column to bring in demographic information to victims and offenders
 
-ALTER TABLE nibrs_victim ADD COLUMN race text;
-ALTER TABLE nibrs_victim_addl ADD COLUMN race text;
-ALTER TABLE nibrs_offender ADD COLUMN race text;
-ALTER TABLE nibrs_offender_addl ADD COLUMN race text;
+## COME BACK AND MAKE ALL THESE SQL LINES INDENTED LIKE THE FOLLOWING:
 
-UPDATE nibrs_victim
-    SET race = 
+ALTER TABLE 
+    nibrs_victim 
+ADD COLUMN 
+    race text;
+ALTER TABLE 
+    nibrs_victim_addl 
+ADD COLUMN 
+race text;
+ALTER TABLE 
+    nibrs_offender 
+ADD COLUMN 
+    race text;
+ALTER TABLE 
+    nibrs_offender_addl 
+ADD COLUMN 
+    race text;
+
+UPDATE 
+    nibrs_victim
+SET 
+    race = 
         (SELECT race_desc 
         FROM ref_race 
         WHERE ref_race.race_id = nibrs_victim.race_id) 
@@ -111,8 +127,151 @@ UPDATE nibrs_incident_addl
     SET incident_date = 
         (SUBSTR(incident_date, 7, 10) || '-' || SUBSTR(incident_date, 4, 5) || '-' || SUBSTR(incident_date, 1, 2));
 
+-- Remove extra 2-digit year prefix from leftover date substring text
+
+UPDATE nibrs_incident_addl
+    SET incident_date =
+        REPLACE(incident_date,'20-','');
+
 -- Format the incident_date from nibrs_incident to drop the empty HH:MM:SS values
 
 UPDATE nibrs_incident
     SET incident_date = 
         DATE(incident_date);
+
+-- Insert Not Applicable value for nulls in Victim Gender feature where victim is not a specific person
+
+UPDATE 
+	crimepred_source 
+SET 
+	"Victim Gender" = 
+		'Not Applicable' 
+WHERE 
+	"Victim Type" IN 
+		('Business', 'Government', 'Religious Organization', 'Society/Public', 'Financial Institution', 'Other')
+AND
+	"Victim Gender" IS NULL
+AND 
+	"Victim Race" IS NULL
+AND 
+	"Victim Age" IS NULL;
+
+-- Insert U value for nulls in Victim Gender where victim is unknown
+UPDATE 
+	crimepred_source
+SET
+	"Victim Gender" = 
+		'U'
+WHERE
+	"Victim Gender" is NULL
+AND
+	"Victim Age" IS NULL
+AND 
+	"Victim Race" =
+		'Unknown';
+
+-- Insert Not Applicable value for nulls in Victim Race where victim is not a specific person
+
+UPDATE 
+	crimepred_source 
+SET 
+	"Victim Race" = 
+		'Not Applicable' 
+WHERE 
+	"Victim Type" IN 
+		('Business', 'Government', 'Religious Organization', 'Society/Public', 'Financial Institution', 'Other')
+AND 
+	"Victim Age" IS NULL
+AND
+    "Victim Race" IS NULL;
+
+-- Insert Unknown value for nulls in Victim Race where victim is unknown
+
+UPDATE
+	crimepred_source
+SET
+	"Victim Race" =
+		'Unknown'
+WHERE
+	"Victim Type" =
+		'Unknown'
+AND
+	"Victim Race" IS NULL;
+
+-- Insert Relationship Unknown value for nulls in Relationship to Offender when victim is unknown, an individual, or a law enforcement officer
+
+UPDATE
+	crimepred_source
+SET
+	"Relation to Offender" =
+		'Relationship Unknown'
+WHERE
+	"Relation to Offender" IS NULL
+AND 
+	"Victim Type" IN 
+		('Unknown', 'Law Enforcement Officer', 'Individual');
+
+-- Insert Not Applicable value for nulls in Relationship to Offender when victim is an organization or society
+
+UPDATE
+	crimepred_source
+SET
+	"Relation to Offender" =
+		'Not Applicable'
+WHERE
+	"Relation to Offender" IS NULL
+AND 
+	"Victim Type" IN 
+		('Business', 'Society/Public', 'Religious Organization', 'Government', 'Financial Institution', 'Other');
+
+-- Insert None value for nulls in Weapon, as null implies no weapon used in incident
+
+UPDATE 
+	crimepred_source 
+SET 
+	"Weapon" = 
+		'None'
+WHERE
+	"Weapon" IS NULL
+
+-- Insert U value for nulls in Victim Gender where victim type is unknown
+
+UPDATE
+	crimepred_source
+SET
+	"Victim Gender" =
+		'U'
+WHERE
+	"Victim Gender" IS NULL
+AND
+	"Victim Type" =
+		'Unknown'
+
+-- Insert U value for nulls in Offender Gender where offender age, race, and gender are unknown
+
+UPDATE 
+	crimepred_source
+SET
+	"Offender Gender" =
+		'U'
+WHERE
+	"Offender Gender" IS NULL
+AND
+	"Offender Race" IS NULL
+AND
+	"Offender Age" IS NULL;
+
+-- Insert Unknown value for nulls in Offender Race where offender race, gender, and age are unknown 
+
+UPDATE
+	crimepred_source
+SET
+	"Offender Race" =
+		'Unknown'
+WHERE
+	"Offender Race" IS NULL
+AND
+	"Offender Gender" =
+		'U'
+AND
+	"Offender Age" IS NULL;
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Crime Prediction with Machine Learning
 ## Author: Frank Serafine | README
 
-Using publicly-available crime data from the FBI, this project aimed to train several machine learning models to be able to predict crime data.
+Using publicly-available crime data from the FBI, this project aimed to train several machine learning models to be able to predict crime data for a given year.
 
-The database constructed in SQLite from the FBI's provided CSV files can be downloaded [from this link](https://drive.google.com/file/d/1OygVhK2Ah8oR5DNsJxNR1V4C8CtN9Pb1/view?usp=sharing), and is approximately 1.5 GB in size.
+The database constructed in SQLite from the FBI's provided CSV files can be downloaded [from this link](https://drive.google.com/file/d/1Q7emKSnD8K-WJrrnZW410LEhxlXoYg63/view?usp=sharing), and is approximately 600 MB zipped / 2.5 GB unzipped.
 
-The final CSV of 5,883,982 crime observations used to train the models, taken from a custom view within this database, can be downloaded [from this link](https://drive.google.com/file/d/16pJ4gI36O67LvdLTRKan774GaBXEcsFB/view?usp=sharing), and is approximately 700 MB.
+The final pre-processed CSV of 6,319,810 observations covering 3,400,357 individual crime offenses is narrowed down in the Jupyter Notebook file in this repo to 3,652 single-day aggregates used to train the models. It can be downloaded [from this link](https://drive.google.com/file/d/16pJ4gI36O67LvdLTRKan774GaBXEcsFB/view?usp=sharing), and is approximately 700 MB.
 
 Files that exist in this repo serve to highlight the work that went in to the project and detail the results.
